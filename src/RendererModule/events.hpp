3e280a2a99cd9294abf4d6e6549ec4975f3ac70d
ShadowCurse <shadowcurse57@gmail.com>
2021-06-25 19:12:53 +0000

    moved some events to render module

---
+++ src/RendererModule/events.hpp
@@ -0,0 +1,14 @@
+#ifndef SLASHENGINE_SRC_RENDERERMODULE_EVENTS_HPP_
+#define SLASHENGINE_SRC_RENDERERMODULE_EVENTS_HPP_
+
+#include "Ecs/entity.hpp"
+
+namespace slash {
+
+struct CreateRenderable {
+  Entity entity;
+};
+
+}
+
+#endif //SLASHENGINE_SRC_RENDERERMODULE_EVENTS_HPP_
