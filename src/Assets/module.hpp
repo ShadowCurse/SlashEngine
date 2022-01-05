798b3953212bcc940e37c34315b64089ef92964c
ShadowCurse <shadowcurse57@gmail.com>
2022-01-05 23:02:03 +0300

    prototyping assets

--- src/Assets/module.hpp
+++ src/Assets/module.hpp
@@ -9,26 +9,13 @@
 
 namespace slash {
 
-  struct AssetsModule;
-
-  template <typename T>
-    struct AssetHandle {};
-  template <typename T>
-    struct Asset {
-      Asset(AssetsModule& am) : am_{am} {}
-
-      auto add(T&& asset) -> AssetHandle<T> {
-        am_.ep_.emit<AssetAdd<T>>(std::forawrd<T>(asset));
-        return AssetHandle<T>{};
-      }
-
-      private:
-        AssetsModule& am_;
-    };
+template <typename T>
+struct AssetHandle {};
+template <typename T>
+struct Asset;
 
 struct AssetsModule : public Dependencies<ResourcePackModule, EventPoolModule> {
-  [[nodiscard]] auto init(ResourcePackModule &rp, EventPoolModule &ep) -> bool {
-
+  [[nodiscard]] auto init(ResourcePackModule& rp, EventPoolModule& ep) -> bool {
     rp.add_resource<Asset<Mesh>>(*this);
     /* ecs_ = &ecs; */
     /* ep_ = &ep; */
@@ -36,19 +23,34 @@ struct AssetsModule : public Dependencies<ResourcePackModule, EventPoolModule> {
     return true;
   }
 
-  [[nodiscard]] auto destroy(ResourcePackModule &rp, EventPoolModule &, SystemModule &, ECSModule &ecs) -> bool {
-
+  [[nodiscard]] auto destroy(ResourcePackModule& rp, EventPoolModule&) -> bool {
     return true;
   }
 
+  template<typename T> [[nodiscard]] auto add(T&& asset) -> AssetHandle<T> {}
+  template<typename T> [[nodiscard]] auto get(AssetHandle<T> handle) -> const T& {}
+  template<typename T> [[nodiscard]] auto get_mut(AssetHandle<T> handle) -> T& {}
+  template<typename T> void remove(AssetHandle<T> handle) {}
 
  private:
   /* ECSModule* ecs_; */
   /* EventPoolModule* ep_; */
 };
 
-}
+template <typename T>
+struct Asset {
+  Asset(AssetsModule& am) : am_{am} {}
 
-#endif // SLASHENGINE_SRC_ASSETS_MODULE_HPP_
+  [[nodiscard]] auto add(T&& asset) -> AssetHandle<T> {
+        return am_.add<T>(std::forward<T>(asset));
+  }
+  [[nodiscard]] auto get(AssetHandle<T> handle) -> const T& { return am_.get<T>(handle); }
+  [[nodiscard]] auto get_mut(AssetHandle<T> handle) -> T& { return am_.get_mut<T>(handle); }
+  void remove(AssetHandle<T> handle) { am_.remove<T>(handle); }
 
+ private:
+  AssetsModule& am_;
+};
+}  // namespace slash
 
+#endif  // SLASHENGINE_SRC_ASSETS_MODULE_HPP_
